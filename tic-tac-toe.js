a6ec1007da56c2ec440f9d350b2740630df3694c
Anna-Kay Graham <graham_annakay@hotmail.com>
2020-10-11 01:32:03 -0500

    Javascript File

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,17 @@
 
 window.onload = function(){
-    const x = document.querySelectorAll("#board div")
+   
     let starter = document.getElementsByClassName("btn");
     starter.addEventListener("click",()=>{
     window.location.reload();
-});
+    });
+    const x = document.getElementById("board")
+    for(var num = 0; num <x.length; num++){
+        x.foreach(i=>{
+            i.setAttribute("class","square")
+            i.setAttribute("mouse",mouse());
+            i.setAttribute("click",click());
+        })
+    }   
+
 };
-for(var num = 0; num <x.length; num++){
-    x=document.getElementById("board").classList.add("square");
-    x.foreach(i=>{
-        i.setAttribute("mouse",mouse());
-        i.setAttribute("click",click());
-    })
-}
